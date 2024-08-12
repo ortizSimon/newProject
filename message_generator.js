805fa1928096ddab35372e1047322aa8ed655658
Simon <ortiz.simon211@gmail.com>
2024-08-11 20:21:29 -0500

    last commit

--- message_generator.js
+++ message_generator.js
@@ -1,1 +1,24 @@
-console.log('Messi is the best football player of all time');
+// Define the data structure with multiple arrays
+const messageParts = {
+    greetings: ["Hello", "Hi", "Hey", "Greetings"],
+    subjects: ["everyone", "world", "you", "all"],
+    actions: ["hope you're having a great day!", "how are you doing?", "wish you a wonderful day!", "hope all is well!"]
+};
+
+// Function to get a random element from an array
+function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+// Function to generate a random message
+function generateMessage() {
+    const greeting = getRandomElement(messageParts.greetings);
+    const subject = getRandomElement(messageParts.subjects);
+    const action = getRandomElement(messageParts.actions);
+    
+    return `${greeting}, ${subject}! ${action}`;
+}
+
+// Display the generated message
+console.log(generateMessage());
